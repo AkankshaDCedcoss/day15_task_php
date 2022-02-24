b5986454e63bb3824c614a8d4c7b76eee4b52903
AkankshaDCedcoss <akankshadwivedi@cedcommerce.com>
2022-02-24 14:54:22 +0530

    task8 commit

---
+++ src/filter_array.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$array1 = array('c1' => 'Red', 'c2' => 'Green', 'c3' => 'White', 'c4' => 'Black'); 
+$array2 = array('c2', 'c4');
+$result = array_diff_key( $array1, array_flip( $array2));
+print_r($result);
+
+
+?>
